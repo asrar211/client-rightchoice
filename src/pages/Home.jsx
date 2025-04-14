7ec2d39da02fc901c6dfb73a522329413f97d22f
asrar211 <113281948+asrar211@users.noreply.github.com>
2025-04-15 01:46:39 +0530

    added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Category } from "../components/home/Category"
 import { Hero } from "../components/home/Hero"
 import {NewArrivals} from "../components/home/NewArrivals"
 import { Testimonials } from "../components/home/Testimonials"
-import { ScrollBasedVelocityDemo } from "../components/Home/Velocity"
+import { ScrollBasedVelocityDemo } from "../components/home/Velocity"
 export const Home = () => {
     return (
         <>
